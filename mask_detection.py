3ced56946a60bc4774241d84136408c429ae3d26
matheus <dev.santos@outlook.com>
2021-12-05 01:03:58 -0300

    first commit

---
+++ mask_detection.py
@@ -0,0 +1,36 @@
+import cv2
+
+
+xml_hear_face = 'haarcascade_frontalface_alt2.xml'
+xml_hear_mouth = 'haarcascade_mouth.xml'
+
+faceClassifier = cv2.CascadeClassifier(xml_hear_face)
+mouthClassifier = cv2.CascadeClassifier(xml_hear_mouth)
+
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+capture.set(cv2.CAP_PROP_FRAME_COUNT, 15)
+
+
+while not cv2.waitKey(20) & 0xFF == ord("q"):
+    ret, frame_color = capture.read()
+
+    gray = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY)
+
+    faces = faceClassifier.detectMultiScale(gray)
+
+    for x, y, w, h in faces:
+        cv2.rectangle(frame_color, (x,y), (x + w, y + h), (0,255,0), 2)
+
+        mouth_rects = mouthClassifier.detectMultiScale(gray, 1.5, 5)
+
+        for (mx, my, mw, mh) in mouth_rects:
+            if(y < my < y + h):
+                cv2.rectangle(frame_color, (x,y), (x + w, y + h), (0,0,255), 2)
+                cv2.rectangle(frame_color, (mx, my), (mx + mh, my + mw),  (255,0,0), 5)
+                break
+    cv2.imshow('Preview', frame_color)
+
+capture.release()
+cv2.destroyAllWindows()
